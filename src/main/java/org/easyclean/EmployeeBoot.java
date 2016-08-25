de18a40aa1d98df2af2ecd43824f3100ab320b34
rafcasto <rafcasto@gmail.com>
2016-08-25 19:57:01 +1200

    Test

--- src/main/java/org/easyclean/EmployeeBoot.java
+++ src/main/java/org/easyclean/EmployeeBoot.java
@@ -1,19 +1,10 @@
 package org.easyclean;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@EnableDiscoveryClient
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
@@ -25,17 +16,3 @@ public class EmployeeBoot {
 	
 }
 
-
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
-    
-}
