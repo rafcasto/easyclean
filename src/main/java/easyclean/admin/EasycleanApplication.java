a3a7954dce3bee03d083a15e2c9c4b4a81b557b1
rafcasto <rafcasto@gmail.com>
2016-08-27 14:17:36 +1200

    adding project

---
+++ src/main/java/easyclean/admin/EasycleanApplication.java
@@ -0,0 +1,12 @@
+package easyclean.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycleanApplication.class, args);
+	}
+}
