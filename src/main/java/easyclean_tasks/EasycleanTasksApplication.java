f51b5ea1805fdb511adddb949bfd45e70eaf384d
rafcasto <rafcasto@gmail.com>
2016-09-10 00:25:01 +1200

    first commit

---
+++ src/main/java/easyclean_tasks/EasycleanTasksApplication.java
@@ -0,0 +1,12 @@
+package easyclean_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycleanTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycleanTasksApplication.class, args);
+	}
+}
