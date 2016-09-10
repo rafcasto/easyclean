0bb29a87a3dfa0037a58d8a1382b810cc2f9a100
rafcasto <rafcasto@gmail.com>
2016-09-10 22:06:52 +1200

    adding a restcontroller

--- src/main/java/easyclean/roster/rostering/RosterController.java
+++ src/main/java/easyclean/roster/rostering/RosterController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import easyclean.roster.rostering.model.Clients;
 import easyclean.roster.rostering.model.Roster;
 
-@Controller
+@RestController
 @RequestMapping("/roster")
 public class RosterController {
 	@Autowired
