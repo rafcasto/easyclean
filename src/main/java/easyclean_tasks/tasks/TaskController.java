24d04fb0c18f307e3f93b86f4bcaf46caf42dd5f
rafcasto <rafcasto@gmail.com>
2016-09-10 22:19:53 +1200

    RestController forgotten

--- src/main/java/easyclean_tasks/tasks/TaskController.java
+++ src/main/java/easyclean_tasks/tasks/TaskController.java
@@ -1,13 +1,13 @@
 package easyclean_tasks.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
