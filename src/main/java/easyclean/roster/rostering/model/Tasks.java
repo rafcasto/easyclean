620a064443bed262801e3a641a69bf2908fb0028
rafcasto <rafcasto@gmail.com>
2016-09-11 20:19:02 +1200

    changing string

--- src/main/java/easyclean/roster/rostering/model/Tasks.java
+++ src/main/java/easyclean/roster/rostering/model/Tasks.java
@@ -1,11 +1,11 @@
 package easyclean.roster.rostering.model;
 
 public class Tasks {
- private int id; 
- public int getId() {
+ private String id; 
+ public String getId() {
 	return id;
 }
-public void setId(int id) {
+public void setId(String id) {
 	this.id = id;
 }
 public String getTaskName() {
