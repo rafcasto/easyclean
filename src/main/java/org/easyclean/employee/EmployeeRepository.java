27e035334c051deae4879673444efd43d69533d9
rafcasto <rafcasto@gmail.com>
2016-09-14 21:22:41 +1200

    adding search like

--- src/main/java/org/easyclean/employee/EmployeeRepository.java
+++ src/main/java/org/easyclean/employee/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package org.easyclean.employee;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String>{
-
+	 @Query("{employeeName : {$regex : ?0}}")
+	  List<Employee> findByThePersonsFirstname(String firstname);
 }
