14e8990ae86d22f13b60aed8e3cc866bdf6040a7
rafcasto <rafcasto@gmail.com>
2016-09-16 23:53:42 +1200

    adding programs

---
+++ src/main/java/easyclean/timesheets/Application.java
@@ -0,0 +1,12 @@
+package easyclean.timesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
